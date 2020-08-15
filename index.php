053ac6c215eb82809c05f389b979648c390df000
Joseph Padfield <joseph.padfield@ng-london.org.uk>
2020-08-16 00:23:29 +0100

    Updating code - Clean help modal of examples

--- index.php
+++ index.php
@@ -232,75 +232,7 @@ function buildModal ()
     "Summary" => ' ',
     "Blank Nodes" => ' ',
     "Formatting" => ' ',
-    "Aliases" => '',
-    "Dump" => ' <h6 class="px-3">Most Used Apps</h6>
-                                <ol class="pb-4">
-                                    <li>Watsapp</li>
-                                    <li>Instagram</li>
-                                    <li>Chrome</li>
-                                    <li>Linkedin</li>
-                                </ol>
-                            </div>
-                            <div class="px-3">
-                                <h6 class="pt-3 pb-3 mb-4 border-bottom"><span class="fa fa-android"></span> Suggested Apps</h6>
-                                <h6 class="text-primary pb-2"><a href="#">Opera Browser</a> <span class="text-secondary">- One of the best browsers</span></h6>
-                                <h6 class="text-primary pb-2"><a href="#">Camscanner</a> <span class="text-secondary">- Easily scan your documents</span></h6>
-                                <h6 class="text-primary pb-4"><a href="#">Coursera</a> <span class="text-secondary">- Learn online, lecturers from top universities</span></h6>
-		<div class="line"></div>		 
-                                <form>
-                                    <div class="form-group pb-5 px-3"> <select name="account" class="form-control">
-                                            <option selected disabled>Select Product</option>
-                                            <option>Product 1</option>
-                                            <option>Product 2</option>
-                                            <option>Product 3</option>
-                                            <option>Product 4</option>
-                                        </select> </div>
-                                </form>
-                            </div>
-                            <div class="px-3">
-                                <h6 class="pt-3 pb-3 mb-4 border-bottom"><span class="fa fa-star"></span> Popular Topics</h6>
-                                <h6 class="text-primary pb-2"><a href="#">Getting started with Blazemeter</a></h6>
-                                <h6 class="text-primary pb-2"><a href="#">Creating tests</a></h6>
-                                <h6 class="text-primary pb-4"><a href="#">Running tests</a></h6>
-		<div class="line"></div>		
-                                <form>
-                                    <div class="form-group pb-5 px-3 row justify-content-center"> <button type="button" class="btn btn-primary">New Community +</button> </div>
-                                </form>
-                            </div>
-                            <div class="px-3">
-                                <div class="border border-1 box">
-                                    <h5>Community 1</h5>
-                                    <p class="text-muted mb-1">Members : <strong>27</strong></p>
-                                </div>
-                                <div class="border border-1 box">
-                                    <h5>Community 2</h5>
-                                    <p class="text-muted mb-1">Members : <strong>16</strong></p>
-                                </div>
-				<div class="line"></div>
-				                               <form>
-                                    <div class="form-group pb-2 px-3"> <input type="text" placeholder="Enter College Name" class="form-control"> </div>
-                                    <div class="form-group row pb-2 px-3">
-                                        <div class="col-6"> <input type="text" placeholder="Percentage" class="form-control"> </div>
-                                        <div class="col-6"> <input type="text" placeholder="Grade" class="form-control"> </div>
-                                    </div>
-                                    <div class="form-group px-3 pb-2"> <label class="form-control-label">
-                                            <h6>What are you good at ?</h6>
-                                        </label>
-                                        <div class="custom-control custom-checkbox"> <input class="custom-control-input" id="option1" type="checkbox" value=""> <label class="custom-control-label" for="option1">Web Development</label> </div>
-                                        <div class="custom-control custom-checkbox"> <input class="custom-control-input" id="option2" type="checkbox" value=""> <label class="custom-control-label" for="option2">Data Structures & Algorithms</label> </div>
-                                        <div class="custom-control custom-checkbox"> <input class="custom-control-input" id="option3" type="checkbox" value=""> <label class="custom-control-label" for="option3">Android Development</label> </div>
-                                        <div class="custom-control custom-checkbox"> <input class="custom-control-input" id="option4" type="checkbox" value=""> <label class="custom-control-label" for="option4">Blockchain</label> </div>
-                                        <div class="custom-control custom-checkbox"> <input class="custom-control-input" id="option5" type="checkbox" value=""> <label class="custom-control-label" for="option5">Machine Learning Algorithms</label> </div>
-                                    </div>
-                                    <div class="form-group pb-5 row justify-content-center"> <button type="button" class="btn btn-primary px-3">Submit</button> </div>
-                                </form>
-                            </div>
-                            <div class="px-3">
-                                <h6 class="pt-3 pb-3 mb-4 border-bottom"><span class="fa fa-rocket"></span> Trending Technologies</h6>
-                                <h6 class="text-primary pb-2"><a href="#">Augmented Reality and Virtual Reality</a></h6>
-                                <h6 class="text-primary pb-2"><a href="#">Angular and React</a></h6>
-                                <h6 class="text-primary pb-2"><a href="#">Big Data and Hadoop</a></h6>
-                                <h6 class="text-primary pb-4"><a href="#">Internet of Things (IoT)</a></h6>',
+    "Aliases" => ''
     );
 
   $tHeaders = false;
